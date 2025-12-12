557af1ab10a8fe22dbab4a4da712f15f4b7ff07b
bouaouedhind-prog <bouaouedhind@gmail.com>
2025-12-13 00:58:49 +0100

    Update Read.rb

--- Read.rb
+++ Read.rb
@@ -16,8 +16,10 @@ print(df, "\n")
 # 2) Sélectionner uniquement la colonne longueur 
 print("Colonne Longueur :") 
 print(df["Longueur"], "\n") 
+
 # 3) Filtrer les séquences dont la longueur › 10 print("Séquences de longueur › 10 :")
 print(df[df["Longueur"] > 10], "\n")
+
 # 4) Calculer le %GC moyen avec 3 chiffres après la virgule
 gc_mean = round(df["Pourcentage_GC"] -mean),
 print("pourcentage moyen de GC :",gc mean ,"\n")
